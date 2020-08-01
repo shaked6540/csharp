d354d4c2fd1d83e894486a4a445d707f6ea73ccb
shaked6540 <shaked6540@users.noreply.github.com>
2020-08-01 20:44:23 +0300

    Finished autocomplete

--- csharp/AutoCompletionHandler.cs
+++ csharp/AutoCompletionHandler.cs
@@ -10,7 +10,7 @@ namespace csharp
     {
         private ScriptRunner script;
 
-        public char[] Separators { get; set; } = new char[] { ' ', '.', ';' };
+        public char[] Separators { get; set; } = new char[] { ' ', '.', ';', '(', '[', '{' };
 
         public AutoCompletionHandler(ScriptRunner script)
         {
@@ -21,13 +21,15 @@ namespace csharp
         // index - The index of the terminal cursor within {text}
         public string[] GetSuggestions(string text, int index)
         {
-            var statements = GetStatements(text);
 
-            if (statements.Count > 0)
+            string context = GetContext(text);
+            List<string> types = GetTypes(context);
+
+            if (types.Count > 0)
             {
-                string first = statements.FirstOrDefault();
+                string first = types.FirstOrDefault();
                 bool withStatic = true;
-                statements.RemoveAt(0);
+                types.RemoveAt(0);
 
                 Type currentType = null;
 
@@ -38,29 +40,29 @@ namespace csharp
                     withStatic = false;
                 }
 
-                // there was only 1 statement
-                if (statements.Count == 0)
+                // there was only 1 type
+                if (types.Count == 0)
                 {
                     return script.LoadedTypes.Where(x => x.Name.StartsWith(first, StringComparison.OrdinalIgnoreCase)).Select(x => Utilities.GetFullTypeName(x)).Distinct().ToArray();
                 }
 
-                // there were only 2 statements
-                else if (statements.Count == 1)
+                // there were only 2 types
+                else if (types.Count == 1)
                 {
                     currentType ??= script.LoadedTypes.FirstOrDefault(x => x.Name == first);
-                    return currentType?.GetSuggestableMemberInfos(withStatic, script).Where(x => x.Name.StartsWith(statements.LastOrDefault(), StringComparison.OrdinalIgnoreCase)).Select(x => x.Name).ToArray();
+                    return currentType?.GetSuggestableMemberInfos(withStatic, script).Where(x => x.Name.StartsWith(types.LastOrDefault(), StringComparison.OrdinalIgnoreCase)).Select(x => x.Name).ToArray();
                 }
 
-                // there were more than 2 statements
-                else if (statements.Count > 1)
+                // there were more than 2 types
+                else if (types.Count > 1)
                 {
-                    string last = statements.LastOrDefault();
-                    statements.RemoveAt(statements.Count - 1);
+                    string last = types.LastOrDefault();
+                    types.RemoveAt(types.Count - 1);
                     currentType ??= script.LoadedTypes.FirstOrDefault(x => x.Name == first);
-                    while (statements.Count > 0)
+                    while (types.Count > 0)
                     {
-                        currentType = currentType?.GetSuggestableMemberInfos(withStatic, script).FirstOrDefault(x => x.Name == statements.FirstOrDefault())?.GetUnderlyingType();
-                        statements.RemoveAt(0);
+                        currentType = currentType?.GetSuggestableMemberInfos(withStatic, script).FirstOrDefault(x => x.Name == types.FirstOrDefault())?.GetUnderlyingType();
+                        types.RemoveAt(0);
 
                         if (withStatic)
                         {
@@ -81,7 +83,34 @@ namespace csharp
             }
         }
 
-        private static List<string> GetStatements(string text)
+        private static string GetContext(string text)
+        {
+            string context = text;
+            int count = 0;
+
+            for (int i = context.Length - 1; i >= 0; i--)
+            {
+                if (context[i] == ')')
+                {
+                    count += 1;
+                }
+
+
+                else if (context[i] == '(')
+                {
+                    if (count > 0)
+                    {
+                        count -= 1;
+                    }
+                    else return i == context.Length - 1 ? "" : context[(i + 1)..];
+                }
+            }
+
+
+            return context;
+        }
+
+        private static List<string> GetTypes(string text)
         {
             List<string> result = new List<string>();
             StringBuilder word = new StringBuilder();
@@ -93,7 +122,7 @@ namespace csharp
                     count += 1;
                     if (count == 0)
                     {
-                        result.Add(word.ToString());
+                        result.Add(word.ToString().Trim());
                         word.Clear();
                     }
                 }
@@ -106,14 +135,17 @@ namespace csharp
                     }
                     else return new List<string>();
                 }
-
+                else if (text[i] == '?')
+                {
+                    continue;
+                }
                 else if (count == 0)
                 {
                     if (text[i] == '.')
                     {
                         if (word.Length > 0)
                         {
-                            result.Add(word.ToString());
+                            result.Add(word.ToString().Trim());
                             word.Clear();
                         }
                     }
@@ -126,7 +158,7 @@ namespace csharp
 
             if (word.Length > 0)
             {
-                result.Add(word.ToString());
+                result.Add(word.ToString().Trim());
             }
 
             if (text.LastOrDefault() == '.')
